f0ebb83f0600238d3d24a80e2df114aa8c032347
Daniël te Winkel <bitbucket@te-winkel.nl>
2020-07-12 20:46:27 +0200

    Added pester tests (not working yet).

---
+++ AzureCli.Tests/AzureCli.Tests.ps1
@@ -0,0 +1,29 @@
+﻿Describe "AzureCli" {
+
+	Context "With alias iaz" {
+
+		Mock -ModuleName 'AzureCli' Invoke-AzCli {return "hi"}
+
+		It "Invokes Invoke-AzCli" {
+			$result = Invoke-AzCli
+
+			$result | Should -Be "hi"
+
+			Assert-MockCalled -ModuleName 'AzureCli' Invoke-AzCli
+		}
+
+	}
+
+	Context "With Object Output" {
+		$returnValue = "Hello from az cli"
+		Mock -ModuleName 'AzureCli' az.exe {return "hi"}
+
+		It "Fails for now" {
+			$returnValue = "Hello from az cli"
+			Invoke-AzCli | Should -Be $returnValue
+			Assert-VerifiableMocks
+		}
+
+	}
+
+}
