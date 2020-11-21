13827783199aeb7a1951007cb1fc2ca503acc54c
Daniël te Winkel <bitbucket@te-winkel.nl>
2020-11-21 15:35:11 +0100

    More Tests.

--- build/Test-Gitversion.ps1
+++ build/Test-Gitversion.ps1
@@ -5,6 +5,12 @@ param(
 )
 
 Write-Host $GitVersion[0]
+Write-Host $GitVersion[1]
+Write-Host $GitVersion[2]
+Write-Host $GitVersion[3]
+Write-Host $GitVersion[4]
+
+$GitVersion | ConvertFrom-Json
 
 throw "Quit now :-)"
 
