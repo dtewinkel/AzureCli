2d30334f07a1f7e18566b656d8ab8e37a9e8c688
Daniël te Winkel <bitbucket@te-winkel.nl>
2020-11-22 20:58:07 +0100

    Use more of build script.

--- deployment/pipelines/scripts/Build-Module.ps1
+++ deployment/pipelines/scripts/Build-Module.ps1
@@ -16,7 +16,16 @@ param(
 	[String] $ModuleRootPath = (Join-Path (Resolve-Path (Join-Path $PSScriptRoot '..' '..', '..')) "Modules"),
 
 	[Parameter()]
-	$GitVersionJson
+	[String] $RepositoryName = "Local${ModuleName}PowerShell",
+
+	[Parameter()]
+	[String] $RepositoryPath = (Join-Path (Resolve-Path (Join-Path $PSScriptRoot '..' '..', '..')) "Repositories" $RepositoryName),
+
+	[Parameter()]
+	$GitVersionJson,
+
+	[Parameter()]
+	[Switch] $CleanupRepository
 )
 
 if($GitVersionJson)
@@ -32,36 +41,80 @@ else
 	}
 }
 
-$modulePath = Join-Path $ModuleRootPath $ModuleName
-if(-not (Test-Path $ModuleRootPath))
+try
 {
-	$null = mkdir $ModuleRootPath
-}
+	if(-not (Test-Path $RepositoryPath))
+	{
+		$null = mkdir $RepositoryPath
+	}
 
-if(Test-Path $modulePath)
+	$repo = Get-PSRepository -Name $RepositoryName -ErrorAction SilentlyContinue
+
+	if(-not $repo)
+	{
+		Register-PSRepository -Name $RepositoryName -SourceLocation $RepositoryPath -InstallationPolicy Trusted
+		Write-Verbose "Registered repository '${RepositoryName}' to directory '${RepositoryPath}'."
+	}
+
+	$modulePath = Join-Path $ModuleRootPath $ModuleName
+	if(-not (Test-Path $ModuleRootPath))
+	{
+		$null = mkdir $ModuleRootPath
+	}
+
+	if(Test-Path $modulePath)
+	{
+		Remove-Item $modulePath -Recurse -Force
+	}
+
+	Copy-Item $SourcePath $ModuleRootPath -Recurse
+
+	$moduleData = Join-Path $modulePath "${ModuleName}.psd1"
+
+	$version = $Gitversion.MajorMinorPatch
+	$nugetPath = (join-Path $repositoryPath "${ModuleName}.${version}.nupkg")
+	$year = ([DateTime]($GitVersion.CommitDate)).Year
+	$copyright = "Copyright © ${year}, ${CompanyName}. All rights reserved."
+	$updateParameters = @{
+		Path = $moduleData
+		ModuleVersion = $GitVersion.MajorMinorPatch
+		Copyright = $copyright
+		Author = $Author
+		CompanyName = $CompanyName
+	}
+	if($GitVersion.NuGetPreReleaseTagV2 -ne "")
+	{
+		$preReleaseTag = $GitVersion.NuGetPreReleaseTagV2 -replace '[^a-zA-Z0-9]', ''
+		$updateParameters.Add("Prerelease", $preReleaseTag)
+		$nugetPath = (join-Path $repositoryPath "${ModuleName}.${version}-${preReleaseTag}.nupkg")
+	}
+	Update-ModuleManifest @updateParameters
+	# make sure the ModuleManifest is in the right encoding.
+	$manifest = Get-Content $moduleData
+	$manifest | Out-File -Encoding utf8BOM $moduleData
+
+	if (Test-Path $nugetPath)
+	{
+		Remove-Item $nugetPath -Force
+	}
+
+	Publish-Module -Path $modulePath -Repository $repositoryName -Force
+
+	if (-not (Test-Path $nugetPath))
+	{
+		throw "${nugetPath} was not created!"
+	}
+}
+finally
 {
-	Remove-Item $modulePath -Recurse -Force
-}
+	if($removeRepo)
+	{
+		$repo = Get-PSRepository -Name $repositoryName -ErrorAction SilentlyContinue
 
-Copy-Item $SourcePath $ModuleRootPath -Recurse
-
-$moduleData = Join-Path $modulePath "${ModuleName}.psd1"
-
-$year = ([DateTime]($GitVersion.CommitDate)).Year
-$copyright = "Copyright © ${year}, ${CompanyName}. All rights reserved."
-$updateParameters = @{
-	Path = $moduleData
-	ModuleVersion = $GitVersion.MajorMinorPatch
-	Copyright = $copyright
-	Author = $Author
-	CompanyName = $CompanyName
+		if($repo)
+		{
+			Unregister-PSRepository -Name $repositoryName
+			Write-Verbose "Unregistered repository '${repositoryName}'."
+		}
+	}
 }
-if($GitVersion.NuGetPreReleaseTagV2 -ne "")
-{
-	$preReleaseTag = $GitVersion.NuGetPreReleaseTagV2 -replace '[^a-zA-Z0-9]', ''
-	$updateParameters.Add("Prerelease", $preReleaseTag)
-}
-Update-ModuleManifest @updateParameters
-# make sure the ModuleManifest is in the right encoding.
-$manifest = Get-Content $moduleData
-$manifest | Out-File -Encoding utf8BOM $moduleData
