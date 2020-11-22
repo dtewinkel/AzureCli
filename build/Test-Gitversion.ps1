cd0622c9728cbf7babef54e73eeca29bb12f19a8
Daniël te Winkel <bitbucket@te-winkel.nl>
2020-11-22 11:03:13 +0100

    Removed test code.

--- build/Test-Gitversion.ps1
+++ build/Test-Gitversion.ps1
@@ -4,12 +4,6 @@ param(
 	$GitVersion
 )
 
-Write-Host $GitVersion[0]
-Write-Host $GitVersion[1]
-Write-Host $GitVersion[2]
-Write-Host $GitVersion[3]
-Write-Host $GitVersion[4]
-
 $GitVersion | ConvertFrom-Json
 
 throw "Quit now :-)"
