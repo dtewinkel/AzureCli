ee773a1c84a494ea9e7b2b599032b312bc8b985f
Daniël te Winkel <bitbucket@te-winkel.nl>
2020-11-27 19:47:06 +0100

    Allow for minimum version of a module.

--- deployment/pipelines/scripts/Install-Modules.ps1
+++ deployment/pipelines/scripts/Install-Modules.ps1
@@ -1,9 +1,30 @@
-$moduleNames = @("PSScriptAnalyzer", "Pester")
-foreach($moduleName in $moduleNames)
-{
-	$module = Get-Module $moduleName -ListAvailable
-	if(-not $module)
-	{
-		Install-Module $moduleName -Scope CurrentUser -Force -PassThru
+@("PSScriptAnalyzer", "Pester@5.1.0") | ForEach-Object {
+	$moduleSpec = $_ -split '@'
+	switch ($moduleSpec.Length) {
+		1 {
+			$getParams = @{
+				Name = $moduleSpec[0]
+			}
+			$installParams = @{
+				Name = $moduleSpec[0]
+			}
+		}
+		2 {
+			$getParams = @{
+				FullyQualifiedName = @{
+					ModuleName = $moduleSpec[0];
+					ModuleVersion = $moduleSpec[1]
+				}
+			}
+			$installParams = @{
+				Name = $moduleSpec[0];
+				MinimumVersion = $moduleSpec[1]
+			}
+		}
+
+	}
+	$module = Get-Module @getParams -ListAvailable
+	if (-not $module) {
+		Install-Module @installParams -Scope CurrentUser -Force -PassThru
 	}
 } | Format-Table -AutoSize
