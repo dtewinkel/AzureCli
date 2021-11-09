f688250d4629c8e553baf1f2fc4c73a121dba2cf
Daniël te Winkel <bitbucket@te-winkel.nl>
2021-11-09 11:49:46 +0100

    Fixed error with case in filename while loading module on case-sensitive file-systems.

--- AzureCli/AzureCli.psm1
+++ AzureCli/AzureCli.psm1
@@ -3,7 +3,7 @@ Get-ChildItem (Join-Path $PSScriptRoot ArgumentsProcessing *.ps1) | ForEach-Obje
 Get-ChildItem (Join-Path $PSScriptRoot Validation *.ps1) | ForEach-Object FullName | Resolve-Path | Import-Module
 Get-ChildItem (Join-Path $PSScriptRoot *.ps1) | ForEach-Object FullName | Resolve-Path | Import-Module
 
-$moduleData = Import-PowerShellDataFile (Join-Path $PSScriptRoot AzureCLi.psd1)
+$moduleData = Import-PowerShellDataFile (Join-Path $PSScriptRoot AzureCli.psd1)
 
 $moduleData.FunctionsToExport | ForEach-Object { Export-ModuleMember -Function $PSItem }
 $moduleData.AliasesToExport | ForEach-Object { Export-ModuleMember -Alias $PSItem }
