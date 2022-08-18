abed5b33cfead2a7b767b8a79820317ddf51349b
Daniël te Winkel <bitbucket@te-winkel.nl>
2022-08-18 11:16:47 +0200

    More syncing up.

--- deployment/pipelines/scripts/ConfigureBuild.ps1
+++ deployment/pipelines/scripts/ConfigureBuild.ps1
@@ -2,9 +2,9 @@
 param()
 
 Write-Host "Configuring..."
-. $PSScriptRoot/Configure.ps1
+& $PSScriptRoot/Configure.ps1
 Write-Host "Building..."
-. $PSScriptRoot/Build.ps1 -CleanupRepository
+& $PSScriptRoot/Build.ps1 -CleanupRepository
 Write-Host "Analyzing scripts..."
-. $PSScriptRoot/AnalyzeScripts.ps1
+& $PSScriptRoot/AnalyzeScripts.ps1
 Write-Host "Done!"
