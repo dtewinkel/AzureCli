a2f958387ba9b6b6de594cdfc28db932ae89d442
Daniël te Winkel <daniel.tewinkel@transavia.com>
2022-09-03 16:58:44 +0200

    Fix code. Add tests.

--- Completion/complete.ps1
+++ Completion/complete.ps1
@@ -4,11 +4,12 @@ function Complete
 	param ($Command, $Cursor = -1)
 
 	$cursorPos = $Cursor -eq -1 ? $Command.Length : $Cursor
-	$pos = $Command.IndexOf('-')
-	if ($pos -eq -1)
+	$pos = $Command.IndexOf('-') -1
+	if ($pos -le -1)
 	{
 		$pos = $Command.Length
 	}
+
 	if ($pos -ge $cursorPos)
 	{
 		$pos = $Command.LastIndexOf(' ', $cursorPos)
@@ -18,12 +19,27 @@ function Complete
 		}
 		$pos = $pos -eq -1 ? 0 : $pos
 	}
+
+	$filter = '*'
+	if($cursorPos -gt 0)
+	{
+		if($command[$cursorPos - 1] -ne ' ')
+		{
+			$filterStart = $command.LastIndexOf(' ', $cursorPos - 1) + 1
+			if($filterStart -le 0)
+			{
+				$filterStart = 0
+			}
+			$filter = $Command.Substring($filterStart, $cursorPos - $filterStart) + '*'
+		}
+	}
+
 	$toSend = $pos -eq 0 ? "" : $Command.Substring(0, $pos)
 	[PSCustomObject]@{
 		Command   = $Command
 		CursorPos = $cursorPos
 		Pos       = $pos
 		ToSend    = $toSend
-		Filter    = ""
+		Filter    = $filter
 	}
 }
--- Completion/complete.tests.ps1
+++ Completion/complete.tests.ps1
@@ -1,16 +1,30 @@
-
 Describe "Complete" {
 
 	BeforeAll {
-		. ./complete.ps1
+		. $PSScriptRoot/complete.ps1
 	}
 
-	It "Work" {
+	Context "with cursor at end of parameters" {
 
-		$result = Complete "aaa bbb"
+		It "uses [<commands>] with filter [<filter>] for parameters [<parameters>]" -TestCases @(
+			@{ parameters = ""; commands = ""; filter = "*" }
+			@{ parameters = "aaa"; commands = ""; filter = "aaa*" }
+			@{ parameters = "aaa "; commands = "aaa"; filter = "*" }
+			@{ parameters = "aaa bbb"; commands = "aaa"; filter = "bbb*"}
+			@{ parameters = "aaa bbb "; commands = "aaa bbb"; filter = "*" }
+			@{ parameters = "aaa bbb -ccc"; commands = "aaa bbb"; filter = "-ccc*" }
+			@{ parameters = "aaa bbb -ccc "; commands = "aaa bbb"; filter = "*" }
+			@{ parameters = "aaa bbb -ccc --"; commands = "aaa bbb"; filter = "--*" }
+			@{ parameters = "aaa bbb -ccc -ddd "; commands = "aaa bbb"; filter = "*" }
+		) {
+			param($parameters, $commands, $filter)
 
-		Write-Host $result
+			$result = Complete $parameters
 
-		$result.ToSend | Should -Be "aaa"
+			Write-Host $result
+
+			$result.ToSend | Should -Be $commands -Because "commands should match"
+			$result.Filter | Should -Be $filter -Because "filter should match"
+		}
 	}
 }
